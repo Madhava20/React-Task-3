7152ff69a899bbff6896aa1d7fd694752b5f605d
Madhava Reddy <92641734+Madhava20@users.noreply.github.com>
2023-10-23 13:23:39 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,67 +1,21 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import ColorPicker from './ColorPicker'; // Import the ColorPicker component
 
 function App() {
-  const [data, setData] = useState([]);
-
-  const fetchData = () => {
-    fetch(`https://dummyjson.com/users`)
-      .then((response) => response.json())
-      .then((actualData) => {
-        console.log(actualData);
-        setData(actualData.users);
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  };
-
-  useEffect(() => {
-    fetchData();
-    // eslint-disable-next-line
-  }, []);
+  const colors = [
+    'red', 'blue', 'green', 'yellow', 'orange', 
+    'purple', 'pink', 'cyan', 'magenta', 'brown', 
+    'gray', 'lightblue', 'lightgreen', 'lavender', 'teal', 
+    'violet', 'gold', 'turquoise', 'indigo', 'lime'
+  ];
 
   return (
     <div className="App">
-      <table>
-        <tbody>
-          <tr>
-            <th>id</th>
-            <th>Profile</th>
-            <th>FirstName</th>
-            <th>LastName</th>
-            <th>Age</th>
-            <th>Gender</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>UserName</th>
-            <th>Password</th>
-            <th>Birthdate</th>
-            <th>Domain</th>
-            <th>Ip</th>
-          </tr>
-          {data.map((users, index) => (
-            <tr key={index}>
-              <td>{users.id}</td>
-              <td>
-                <img src={users.image} alt="" height={50} />
-              </td>
-              <td>{users.firstName}</td>
-              <td>{users.lastName}</td>
-              <td>{users.age}</td>
-              <td>{users.gender}</td>
-              <td>{users.email}</td>
-              <td>{users.phone}</td>
-              <td>{users.username}</td>
-              <td>{users.password}</td>
-              <td>{users.birthDate}</td>
-              <td>{users.domain}</td>
-              <td>{users.ip}</td>             
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <header className="App-header">
+        {/* Other content */}
+        <ColorPicker colors={colors} /> {/* Pass the colors array as a prop */}
+      </header>
     </div>
   );
 }
